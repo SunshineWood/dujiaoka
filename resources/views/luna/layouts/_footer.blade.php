006637e34f2c36b9a609c8dee4e0016856e4d449
Sunhaonan <haonan.sun@yunzhisun.com>
2025-01-15 18:08:39 +0800

    update ICP

--- resources/views/luna/layouts/_footer.blade.php
+++ resources/views/luna/layouts/_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="layui-row">
         <div class="layui-col-md8 layui-col-md-offset2 layui-col-sm12">
             <div style="text-align: center">{!! dujiaoka_config_get('footer') !!}</div>
-            <p>Powered by <a href="https://github.com/assimon/dujiaoka" target="_blank">@独角数卡.DJK</a></p>
+            <p><a href="http://beian.miit.gov.cn/" target="_blank">京ICP备2025105153号-1</a></p>
         </div>
     </div>
 </div>
